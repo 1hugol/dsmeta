391dfa31af0e0378d7d97c90fc972d3bfc7f7fb1
1hugol <1hugoamorim@gmail.com>
2022-11-14 09:39:32 -0300

    Project created

---
+++ backend/src/test/java/com/hdvelopers/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdvelopers.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
